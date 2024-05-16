b4f331daa7876487f626992554ce39aa742dcb1b
Drxco <156522801+DrxcoDev@users.noreply.github.com>
2024-05-16 16:19:31 +0200

    Footer.tsx, change list of links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,8 +14,8 @@ const Footer: React.FC = () => {
           <div className="w-full md:w-1/2 text-center md:text-right mt-4 md:mt-0">
             <h2 className="text-lg font-bold">Links</h2>
             <ul className="list-none">
-              <li><a href="#" className="text-white hover:underline">Home</a></li>
-              <li><a href="#" className="text-white hover:underline">About</a></li>
+              <li><a href="/" className="text-white hover:underline">Home</a></li>
+              <li><a href="/dashboard" className="text-white hover:underline">Dashboard</a></li>
               <li><a href="#" className="text-white hover:underline">Services</a></li>
               <li><a href="#" className="text-white hover:underline">Contact</a></li>
             </ul>
